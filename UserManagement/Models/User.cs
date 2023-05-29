914528305d9cda2b16e9abcc0b3cb8a4c9b70c66
nithyapriyav-kanini <127306319+nithyapriyav-kanini@users.noreply.github.com>
2023-05-29 11:45:59 +0530

    Add files via upload

---
+++ UserManagement/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Models
+{
+    public class User
+    {
+        [Key]
+        public string Username { get; set; }
+        public byte[]? Password { get; set; } 
+        public byte[]? HashKey { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public long PhoneNo { get; set; }
+        public string Role { get; set; }
+    }
+}
