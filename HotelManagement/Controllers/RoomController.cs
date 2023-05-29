551e4b6c5e2dcc1db6e5172af17601d9bc7966ac
nithyapriyav-kanini <127306319+nithyapriyav-kanini@users.noreply.github.com>
2023-05-29 11:51:22 +0530

    Add files via upload

---
+++ HotelManagement/Controllers/RoomController.cs
@@ -0,0 +1,78 @@
+﻿using HotelManagement.Models;
+using HotelManagement.Models.DTO;
+using HotelManagement.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        RoomService _service;
+        public RoomController(RoomService roomService)
+        {
+            _service = roomService;
+        }
+        [HttpGet("All")]
+        public ActionResult<List<Room>> GetAll(string name)
+        {
+            List<Room> list = new List<Room>();
+            list=_service.GetAll(name).ToList();
+            if(list.Count != 0&&list!=null)
+            {
+                return Ok(list);
+            }
+            return BadRequest("Rooms not available");
+        }
+        [HttpGet("one")]
+        public ActionResult<Room> Get(string name,int r_no)
+        {
+            Room room = new Room();
+            room=_service.Get(name, r_no);
+            if(room!=null)
+            {
+                return Ok(room);
+            }
+            return BadRequest("There is no such room available");
+        }
+        [HttpDelete]
+        [Authorize(Roles = "admin")]
+        public ActionResult<Room> Remove(string name,int r_no)
+        {
+            Room room=new Room();
+            room = _service.Remove(name, r_no);
+            if(room != null)
+            {
+                return Ok(room);
+            }
+            return BadRequest("Can't remove that room");
+        }
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public ActionResult<Room> Add(RoomDTO room)
+        {
+            Room room1 = new Room();
+            room1= _service.Add(room);
+            if( room1!=null)
+            {
+                return Ok(room1);
+            }
+            return BadRequest("can't add the room");
+        }
+        [HttpPut]
+        [Authorize(Roles = "admin")]
+        public ActionResult<Room> Update(string name,int r_no,RoomDTO room)
+        {
+            Room room1=new Room();
+            room1=_service.Update(name, r_no,room);
+            if (room1 != null)
+            {
+                return Ok(room1);
+            }
+            return BadRequest("Can't update this room");
+        }
+    }
+}
